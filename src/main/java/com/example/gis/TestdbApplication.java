2ad051e33a4da5d7f5fc856d7c3c1543099793be
murray <you@example.com>
2018-01-01 16:02:07 +0000

    remove unused and tabs to spaces

--- src/main/java/com/example/gis/TestdbApplication.java
+++ src/main/java/com/example/gis/TestdbApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class TestdbApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(TestdbApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(TestdbApplication.class, args);
+    }
 }
